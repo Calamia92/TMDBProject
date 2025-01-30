0a33182ecaecd29e6f33fac82485e5da0043b8e5
Calamia92 <119439868+Calamia92@users.noreply.github.com>
2025-01-30 12:11:14 +0100

    Init supabase connect

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from supabase import create_client, Client
+import config
 
 app = Flask(__name__)
 
+supabase: Client = create_client(config.SUPABASE_URL, config.SUPABASE_KEY)
 
+@app.route('/users')
+def get_users():
+    response = supabase.table("users").select("*").execute()
+    return jsonify(response.data)
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def home():
+    return "Bienvenue sur l'API Flask avec Supabase!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
