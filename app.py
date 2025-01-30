2f8eaec2d7916c24a45a052a29ec4576b7614913
Calamia92 <119439868+Calamia92@users.noreply.github.com>
2025-01-30 15:11:49 +0100

    UPDATE FRONT UI

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 import requests
-from flask import Flask, jsonify
+from flask import Flask, render_template,jsonify, request, redirect, url_for
 from dotenv import load_dotenv
 from supabase import create_client, Client
-
 
 load_dotenv()
 
@@ -23,47 +22,35 @@ TMDB_BASE_URL = "https://api.themoviedb.org/3"
 
 @app.route("/")
 def home():
-    return "Bienvenue sur l'API Flask avec TMDB !"
-
-@app.route('/users')
-def get_users():
-    response = supabase.table("users").select("*").execute()
-    return jsonify(response.data)
+    return render_template("home.html")
 
 
-@app.route("/trending")
-def get_trending_movies():
-    """Récupère les films tendances de la semaine"""
-    url = f"{TMDB_BASE_URL}/trending/movie/week"
-    headers = {"Authorization": f"Bearer {TMDB_READ_ACCESS_TOKEN}"}
+@app.route('/movies/<path:movie_id>')
+def get_movie_by_id(movie_id):
+    try:
+        movie_id = int(movie_id)
+    except ValueError:
+        return jsonify({"error": "L'ID du film doit être un entier valide."}), 400
 
+    url = f"{TMDB_BASE_URL}/movie/{movie_id}"
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        return jsonify(response.json())
-    else:
-        return jsonify({"error": "Erreur TMDB", "status_code": response.status_code, "message": response.text})
-
-@app.route('/movies/<int:movie_id>')
-def get_movie_by_id(movie_id):
-    """Récupère un film par son ID TMDB"""
-    url = f"{TMDB_BASE_URL}/movie/{movie_id}" 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:  
         data = response.json()
-        filtered_data = {
-            "title": data.get("title"),
-            "release_date": data.get("release_date"),
-            "genres": [genre["name"] for genre in data.get("genres", [])],
-            "popularity": data.get("popularity"),
-            "vote_average": data.get("vote_average"),
-        }  
-        return jsonify(filtered_data)
-    
-    else:    
-        return jsonify({"error": "Erreur TMDB", "status_code": response.status_code, "message": response.text})
-        
+        return render_template("movie.html", movie=data)
+    else:
+        return render_template("movie.html", movie=None, error="Film non trouvé.")
+
+
+@app.route("/movies/", methods=["GET"])
+def search_movie():
+    """Redirige vers la bonne URL avec l'ID du film"""
+    movie_id = request.args.get("movie_id")
+
+    if not movie_id or not movie_id.isdigit():
+        return jsonify({"error": "Veuillez entrer un ID de film valide."}), 400
+
+    return redirect(url_for("get_movie_by_id", movie_id=int(movie_id)))
 
 if __name__ == '__main__':
     app.run(debug=True)
